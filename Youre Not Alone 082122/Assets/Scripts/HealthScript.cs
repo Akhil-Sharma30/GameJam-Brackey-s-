98001fce8d4a59f28ad9a843baa19593e200f5a9
Akhil Sharma <akhilsharma.off@gmail.com>
2022-08-25 23:06:56 +0530

    coin pointer system, enemy follow player system uploaded

---
+++ Youre Not Alone 082122/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+
+    public int curHealth = 0;
+    public int maxHealth = 100;
+    // Start is called before the first frame update public int curHealth = 0;
+   
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        Debug.Log(curHealth);
+    //    if(Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //     DamagePlayer(10);
+    //    }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if(other.transform.tag == "Player")
+        {
+            DamagePlayer(5);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        curHealth -= damage;
+
+        healthBar.SetHealth(curHealth);
+    }
+}
