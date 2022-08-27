46043825be672ee91031a6eff27a5ef255acf9e7
Anant Saxena <anantsaxena1990@gmail.com>
2022-08-27 15:19:54 +0530

    Health system and timer packages added

---
+++ Youre Not Alone 082122/Assets/Anant Assests/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int health = 0;
+    public HealthBar healthBar;
+
+    //[SerializeField] private float maxHealth = 100f;
+
+    public void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void UpdateHealth(int mod)
+    {
+        health -= mod;
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        } else if (health <= 0)
+        {
+            health = 0;
+            Debug.Log("Player Respawn");
+        }
+
+        healthBar.SetHealth(health);
+    }
+}
---
+++ Youre Not Alone 082122/Assets/Anant Assests/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int speed = 3;
+    public int attackDamage = 10;
+    public int attackSpeed = 1;
+    public float canAttack;
+
+    public Transform target;
+
+    public void Update()
+    {
+        if(target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+    public void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (attackSpeed <= canAttack)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(attackDamage);
+                canAttack = 0f;
+            } else
+            {
+                canAttack += Time.deltaTime;
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            target = other.transform;
+
+            Debug.Log(target);
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            target = null;
+        }
+    }
+}
